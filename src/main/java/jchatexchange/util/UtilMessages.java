acd6af5b27758ba2d1ecf1106ba7cee314d147ff
Vincentyfication <vincentyification@gmail.com>
2014-11-12 01:04:46 +0800

    Implements a MessageBundleLoader as specified by #1.

--- src/main/java/jchatexchange/util/UtilMessages.java
+++ src/main/java/jchatexchange/util/UtilMessages.java
@@ -5,26 +5,38 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import com.github.fge.msgsimple.bundle.MessageBundle;
 import com.github.fge.msgsimple.bundle.PropertiesBundle;
+import com.github.fge.msgsimple.load.MessageBundleLoader;
 
-class UtilMessages {
+/**
+ * This class supplies a {@link com.github.fge.msgsimple.bundle.MessageBundle
+ * MessageBundle} for messages used by the objects in the <tt>util</tt> package.<br>
+ * UtilMessages @ jchatexchange.util
+ *
+ * @author Unihedron<<a href="mailto:vincentyification@gmail.com"
+ *         >vincentyification@gmail.com</a>>
+ */
+class UtilMessages implements MessageBundleLoader {
 
     public static MessageBundle bundle;
     private Lock                lock = new ReentrantLock();
 
-    public MessageBundle ex() {
-        if (null == bundle)
-            if (lock.tryLock())
-                try {
-                    bundle = PropertiesBundle.forPath("jchatexchange.exceptions");
-                }
-        finally {
-            lock.unlock();
-            // Ditch the lock, we'll never need it again.
-            lock = null;
-        }
-            else while (null == bundle)
-                ;
+    public MessageBundle getBundle() {
+        if (null != bundle)
+            init();
         return bundle;
+    }
+
+    private void init() {
+        if (lock.tryLock())
+            try {
+                bundle = PropertiesBundle.forPath("jchatexchange.exceptions");
+            }
+            finally {
+                lock.unlock();
+                // Ditch the lock, it's not needed.
+                lock = null;
+            }
+        else lock.lock();
     }
 
 }
